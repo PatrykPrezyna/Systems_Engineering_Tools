bdc6c4efa1d524eb9d3c236523b39923b67fc78e
patrykprezyna <patryk.prezyna@rommelag.com>
2025-02-20 21:47:53 +0100

    combine tradespaces

---
+++ Tradespace_exploration_python/combine_plots.py
@@ -0,0 +1,33 @@
+from PIL import Image
+
+def get_concat_v():
+    print("test")
+    image1 = Image.open(r'Tradespace0.png')
+    image2 = Image.open(r'Tradespace1.png')
+    image3 = Image.open(r'Tradespace2.png')
+    image4 = Image.open(r'Tradespace3.png')
+    image5 = Image.open(r'Tradespace4.png')
+    image6 = Image.open(r'Tradespace5.png')
+
+    im1 = image1.convert('RGB')
+    im2 = image2.convert('RGB')
+    im3 = image3.convert('RGB')
+    im4 = image4.convert('RGB')
+    im5 = image5.convert('RGB')
+    im6 = image6.convert('RGB')
+
+    dst = Image.new('RGB', (im1.width+im2.width, im1.height + im3.height + im5.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    dst.paste(im3, (0, im1.height))
+    dst.paste(im4, (im2.width, im1.height))
+    dst.paste(im5, (0, im1.height+im1.height))
+    dst.paste(im6, (im2.width, im1.height+im1.height))
+    dst.save('Tradespaces.png')
+    return True
+
+
+
+if (__name__ == '__main__'):
+    print('Executing as standalone script')
+    get_concat_v()
--- Tradespace_exploration_python/tradespace.py
+++ Tradespace_exploration_python/tradespace.py
@@ -1,78 +1,61 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import combine_plots
 
 # Load designs.json
 with open('designs.json', 'r') as file:
-# with open('designs_manual.json', 'r') as file:
+#with open('designs_manual.json', 'r') as file:
     designs = json.load(file)
 
-# Extract cost and performance values
+# Define the utopia point (ideal but unattainable point)
 costs = [design['Estimated Cost'] for design in designs]
 performances = [design['Estimated Performance'] for design in designs]
-options = [design['Selected Options'] for design in designs]
-name = [design['Name'] for design in designs]
-print(name)
 points = np.array(list(zip(costs, performances)))
-
-# Define the utopia point (ideal but unattainable point)
 utopia_point = [min(costs), max(performances)]
+
 
 reference_color = ['blue','yellow','red', "black", "yellow", "violet", "grey"]# for each option
 decisions = ["Cutting Plane Control Method", "Robot Mount Type","Pre-op Imaging Type","Procedure Imaging Type","Onboard vs Offboard Power","Onboard vs Offboard Computing"]
 if len(decisions) != len(designs[1]['Selected Options']):#test if the number of decisison is correct
     print("ERORR !!!!!!!!!!!!")
 for j in range(len(designs[1]['Selected Options'])):
-    #create scatter plot for each option
-    decision_to_color = j
     unique_options = set()
     for item in designs:
         unique_options.add(item['Selected Options'][j])
     unique_options = sorted(list(unique_options))
-    print("unique_options"+str(unique_options))
+
     fig = plt.figure(figsize=(10, 6))
     ax = fig.add_subplot(111)
-
     for i, option in enumerate(unique_options):
-        print(option)
-        costs = [design['Estimated Cost'] for design in designs]
-        performances = [design['Estimated Performance'] for design in designs]
-        Tradespace_title = str(decisions[j]) + ": " + str(option)
-        plt.scatter(costs, performances, c=reference_color[i], label=Tradespace_title)
-
-
-    # color = []
-    # for item in designs:
-    #     for i, option in enumerate(unique_options):
-    #         if item['Selected Options'][decision_to_color] == option:
-    #             color.append(reference_color[i])
-
-    #plt.scatter(costs, performances, c=color, label='Tradespace')
+        costs = [design['Estimated Cost'] for design in designs if design['Selected Options'][j]==option]
+        performances = [design['Estimated Performance'] for design in designs if design['Selected Options'][j]==option]
+        names = [design['Name'] for design in designs if design['Selected Options'][j]==option]
+        plot_label = str(option)
+        plt.scatter(costs, performances, c=reference_color[i], label=plot_label)
+        # add label for each design point
+        for i, cost in enumerate(costs):
+            if names[i] != "":
+                label_name = names[i]
+                ax.text(costs[i], performances[i], label_name)
+    plt.scatter(*utopia_point, c='green', s=100, label='Utopia Point')
     # #plt.plot(pareto_points[:, 0], pareto_points[:, 1], 'r--', label='Pareto Frontier')
     # #plt.scatter(pareto_points[:, 0], pareto_points[:, 1], c='red', label='Pareto Points')
-    # # Add the utopia point to the plot
-    plt.scatter(*utopia_point, c='green', s=100, label='Utopia Point')
+    
 
     # Add labels and title
     plt.xlabel('Estimated Cost')
     plt.ylabel('Estimated Performence')
-    combined_array = list(zip(unique_options, reference_color))
-    title = 'Tradespace: ' + str(combined_array)
+    title = 'Tradespace for architectural decision: ' + str(decisions[j])
     plt.title(title)
     plt.legend()
     plt.grid(True)
 
-    # # add label for each design point
-    # for i, txt in enumerate(options):
-    #     if name[i] == "":
-    #         label_name = ' '#+str(i)#label for not design point without name
-    #     else:
-    #         #label_name = ' '+str(i)+'  '+name[i]
-    #         label_name = name[i]
 
-    #     ax.text(costs[i], performances[i], label_name)
-    pdf_name = "Tradespace" + str(j) + ".png"
-    plt.savefig(pdf_name)
+    file_name = "Tradespace" + str(j) + ".png"
+    plt.savefig(file_name)
+
+combine_plots.get_concat_v()
 
 # Show the plot
 # plt.show()
