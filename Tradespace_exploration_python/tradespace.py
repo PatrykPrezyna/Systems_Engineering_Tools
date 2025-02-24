e6ec55121a5e2e2166fbbec08bf1d02e44304ec3
patrykprezyna <patryk.prezyna@rommelag.com>
2025-02-24 12:27:19 +0100

    final changes before submission

--- Tradespace_exploration_python/tradespace.py
+++ Tradespace_exploration_python/tradespace.py
@@ -22,13 +22,22 @@ def is_efficient_efficient(points, min_dim=0, max_dim=1):
 
     return [points[is_efficient], is_efficient]
 
-# Load designs.json
+# Load designs
+with open('selected_designs.json', 'r') as file:
+    selected_designs = json.load(file)
+
 with open('designs.json', 'r') as file:
     designs = json.load(file)
 
 with open('reference_designs.json', 'r') as file:
     reference_designs = json.load(file)
-designs = reference_designs
+
+
+#config:
+show_selected_designs = True
+show_generated_designs = False
+show_reference_designs = True
+#designs = reference_designs
 
 # Define the utopia point (ideal but unattainable point)
 costs = [design['Estimated Cost'] for design in designs]
@@ -54,33 +63,46 @@ for j in range(len(designs[1]['Selected Options'])):
     for item in designs:
         unique_options.add(item['Selected Options'][j])
     unique_options = sorted(list(unique_options))
-
     fig = plt.figure(figsize=(10, 6))
     ax = fig.add_subplot(111)
-    for i, option in enumerate(unique_options):
-        costs = [design['Estimated Cost'] for design in designs if design['Selected Options'][j]==option]
-        y_values = [design[y_axis_values[factor]] for design in designs if design['Selected Options'][j]==option]
-        names = [design['Name'] for design in designs if design['Selected Options'][j]==option]
-        plot_label = str(option)
-        plt.scatter(costs, y_values, c=reference_color[i], label=plot_label, s=6)
-        # add label for each design point
-        # for i, cost in enumerate(costs):
-        #     if names[i] != "":
-        #         label_name = names[i]
-        #         print(label_name)
-        #         ax.text(costs[i], y_values[i], label_name)
+    if show_generated_designs:
+        for i, option in enumerate(unique_options):
+            costs = [design['Estimated Cost'] for design in designs if design['Selected Options'][j]==option]
+            y_values = [design[y_axis_values[factor]] for design in designs if design['Selected Options'][j]==option]
+            names = [design['Name'] for design in designs if design['Selected Options'][j]==option]
+            plot_label = str(option)
+            plt.scatter(costs, y_values, c=reference_color[i], label=plot_label, s=6)
+            # add label for each design point
+            # for i, cost in enumerate(costs):
+            #     if names[i] != "":
+            #         label_name = names[i]
+            #         print(label_name)
+            #         ax.text(costs[i], y_values[i], label_name)
     plt.scatter(*utopia_point, c='gold', s=500, marker="*", label='Utopia Point')
-    #for ref design
-    costs = [design['Estimated Cost'] for design in reference_designs]
-    y_values = [design[y_axis_values[factor]] for design in reference_designs]
-    names = [design['Name'].partition("|")[0] for design in reference_designs]
-    plt.scatter(costs, y_values, c='orange',s=100, marker="X", label="reference designs")
-    # add label for each design point
-    for i, cost in enumerate(costs):
-        if names[i] != "":
-            label_name = "" + str(names[i])
-            print(label_name)
-            ax.text(costs[i], y_values[i], label_name, size=13)  
+    #for selected design
+    if show_selected_designs:
+        costs = [design['Estimated Cost'] for design in selected_designs]
+        y_values = [design[y_axis_values[factor]] for design in selected_designs]
+        names = [design['Name'].partition("|")[0] for design in selected_designs]
+        plt.scatter(costs, y_values, c='red',s=100, marker="X", label="selected designs")
+        # add label for each design point
+        for i, cost in enumerate(costs):
+            if names[i] != "":
+                label_name = "" + str(names[i])
+                print(label_name)
+                ax.text(costs[i], y_values[i], label_name, size=13) 
+    if show_reference_designs:
+        #for ref design
+        costs = [design['Estimated Cost'] for design in reference_designs]
+        y_values = [design[y_axis_values[factor]] for design in reference_designs]
+        names = [design['Name'].partition("|")[0] for design in reference_designs]
+        plt.scatter(costs, y_values, c='orange',s=100, marker="X", label="reference designs")
+        # add label for each design point
+        for i, cost in enumerate(costs):
+            if names[i] != "":
+                label_name = "" + str(names[i])
+                print(label_name)
+                ax.text(costs[i], y_values[i], label_name, size=13)  
 
     pareto = is_efficient_efficient(points)
     pareto_points = pareto[0]
@@ -93,6 +115,8 @@ for j in range(len(designs[1]['Selected Options'])):
     plt.xlabel('Estimated Cost')
     plt.ylabel(y_axis_values[factor])
     title = 'Tradespace for architectural decision: ' + str(decisions[j])
+    if show_generated_designs == False:
+        title = 'Tradespace for architectural decisions'
     plt.title(title)
     plt.legend()
     plt.grid(True)
