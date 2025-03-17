aba7d535e2ef789963a1cc6829346528c70a7e6d
Patryk <patryk.prezyna@protonmail.com>
2025-03-17 11:33:43 +0100

    improve name and aplay min and max to the summed up metric2

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -17,8 +17,9 @@ with open('output_data/designs.json', 'r') as file:
     designs = json.load(file)
 
 # TESTs:
-# Test if the sum of all decisisosn weights per metric adds up to 1
-
+# Test if the sum of all decisisosn weights per metric:
+#a adds up to 1: average calculation 
+#b all are 1 or 2: simple sum
 
 new_designs = []
 for i, design in enumerate(designs): # for every design point
@@ -35,6 +36,10 @@ for i, design in enumerate(designs): # for every design point
                         if option_metric["name"] == metric["name"]:
                             metric_value = min(max(option_metric["mean"], metric["min"]), metric["max"])
                             total_metrics[m] = total_metrics[m] + metric_value*decisions[j]["Weighting " + decision_option["metrics"][m]["name"]]
+    # Apply the min and max also to the summed metric
+    for tm, total_metric in enumerate(total_metrics):
+        total_metrics[tm] = min(max(total_metrics[tm], config["metrics"][tm]["min"]), config["metrics"][tm]["max"])
+        print(total_metrics[tm])
 
     new_design = {
         "Name":design["Name"],
